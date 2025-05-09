86c59e22066e2f291aa5a690c5640ebe40e040a9
Aleksei V <al.voitovskii@gmail.com>
2025-05-09 23:09:58 +0300

    update: add user greeting as 'Anonymous' when --username param value isn't provided

--- src/modules/7-utils.module.js
+++ src/modules/7-utils.module.js
@@ -11,7 +11,7 @@ const getCliUsername = () => {
   let username = null;
   username = process.argv.find(arg => arg.startsWith('--username='))?.split('=').at(-1);
   if (!username) username = process.env.npm_config_username; // https://www.golinuxcloud.com/pass-arguments-to-npm-script/
-  return username;
+  return username || 'Anonymous';
 }
 
 const pathResolveCustom = (cwd, _path) => {
